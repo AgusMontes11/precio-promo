6e572ebe1ac9388e8e710a137081e07d7648cc12
AgusMontes11 <montesagus2001@gmail.com>
2025-12-04 12:51:50 -0300

    Arregllos tema oscuro

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,16 @@ import { BrowserRouter } from "react-router-dom";
 import AppShell from "./AppShell";
 
 export default function App() {
+  
+  useEffect(() => {
+  const savedTheme = localStorage.getItem("theme") || "light";
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark-theme");
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
+}, []);
+
   return (
     <BrowserRouter>
       <AppShell />
