a04d21ffd8b28c15ecfc6aa04491051717cff9bf
AgusMontes11 <montesagus2001@gmail.com>
2025-12-08 19:20:17 -0300

    .env apuntando directo al back nuevo

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://backend-nuevo.montesagus2001.workers.dev",
 });
 
 export default axiosClient;
